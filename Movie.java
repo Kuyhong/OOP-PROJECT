53eee1a03ac1c061bc159ef684c2e3b151ee81f6
Chhuong Sophakvatey <vateychhoung@gmail.com>
2026-01-27 12:53:52 +0700

    done

--- Movie.java
+++ Movie.java
@@ -5,5 +5,12 @@ public class Movie {
     private int duration;
     private String rating;
     private String director;
+        public Movie(int id, String title, String genre, int duration, String rating, String director) {
+            this.movieID = id;
+            this.title = title;
+            this.genre = genre;
+            this.duration = duration;
+            this.rating = rating;
+            this.director = director;
 }
 
--- Showtime.java
+++ Showtime.java
@@ -5,4 +5,11 @@ public class Showtime {
     private String time;
     private int hallNumber;
     private int availableSeats;
+        public Showtime(int id, String movie, String date, String time, int hall, int seats) {
+            this.showtimeID = id;
+            this.movieTitle = movie;
+            this.date = date;
+            this.time = time;
+            this.hallNumber = hall;
+            this.availableSeats = seats;
 }

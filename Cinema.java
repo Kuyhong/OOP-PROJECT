08cca11f075aa874837c5c42cf3fd0ba546daa79
Chea Kuyhong <kuyhongchea.dev@gmail.com>
2026-01-27 11:03:52 +0700

    updated

--- Cinema.java
+++ Cinema.java
@@ -1,3 +1,8 @@
 public class Cinema {
-    
+    private int cinemaID;
+    private String name;
+    private String location;
+    private String contactNumber;
+    private Hall[] halls;
+    private int numberOfHalls;
 }
--- Ticket.java
+++ Ticket.java
@@ -1,3 +1,9 @@
 public class Ticket {
-    
+    private int ticketID;
+    private Showtime showtime;
+    private Movie movie;
+    private Hall hall;
+    private Seat seat;
+    private double price;
+    private String bookingStatus;
 }

bd2a5019a1b31cebaf2a1c110c8d7df0e5c08c63
Chhuong Sophakvatey <vateychhoung@gmail.com>
2026-01-27 13:20:50 +0700

    done

--- Seat.java
+++ Seat.java
@@ -1,3 +1,8 @@
 public class Seat {
-    
-}
+    private String seatID;
+    private boolean isBooked;
+        public Seat(String id) {
+            this.seatID = id;
+            this.isBooked = false;
+        }
+}       

fd86a2954654b570740580eb36be3b264cccfc68
Chhuong Sophakvatey <vateychhoung@gmail.com>
2026-01-27 11:10:25 +0700

    done

--- Movie.java
+++ Movie.java
@@ -1,3 +1,9 @@
 public class Movie {
-    
+    private int movieID;
+    private String title;
+    private String genre;
+    private int duration;
+    private String rating;
+    private String director;
 }
+
--- Showtime.java
+++ Showtime.java
@@ -1,3 +1,8 @@
 public class Showtime {
-    
+    private int showtimeID;
+    private String movieTitle;
+    private String date;
+    private String time;
+    private int hallNumber;
+    private int availableSeats;
 }
